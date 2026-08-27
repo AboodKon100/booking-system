6d7ee09ed3a82c2d0a1ca32b7b8621a2586e7387
AboodKon100 <abdallah.biz100@gmail.com>
2026-08-27 22:55:27 +0300

    Fix Render deployment - db path, table creation, Python 3.11

--- app.py
+++ app.py
@@ -7,8 +7,9 @@ from sqlalchemy import func, and_
 import uuid, random, string, os, re
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.urandom(32)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///noomly.db'
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', os.urandom(32))
+db_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'noomly.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', f'sqlite:///{db_path}')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
@@ -342,7 +343,8 @@ def api_stats():
         'customers': db.session.query(func.count(func.distinct(Appointment.cust_email))).filter_by(business_id=current_user.id).scalar() or 0,
     })
 
+with app.app_context():
+    db.create_all()
+
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
     app.run(debug=True, port=5000)
